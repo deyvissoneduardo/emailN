445f84cf28136d44c1ebea9729fc0adc4250a8c0
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2024-07-09 14:25:40 -0300

    routers authenticated

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,19 +7,32 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	router := chi.NewRouter()
+
+	router.Use(middleware.RealIP)
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
 
 	db := database.NewBD()
 
 	service := campaign.ServiceImpl{Repository: &database.CampaignRepository{Db: db}}
 	handler := endpoints.Handler{CampaignService: &service}
 
-	router.Post("/campaigns", endpoints.HandlerError(handler.CampaignPost))
-	router.Get("/campaigns/{id}", endpoints.HandlerError(handler.CampaignGetById))
-	router.Delete("/campaigns/delete/{id}", endpoints.HandlerError(handler.CampaignDelete))
+	router.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	router.Route("/campaigns", func(router chi.Router) {
+		router.Use(endpoints.Auth)
+		router.Post("/", endpoints.HandlerError(handler.CampaignPost))
+		router.Get("/{id}", endpoints.HandlerError(handler.CampaignGetById))
+		router.Delete("/delete/{id}", endpoints.HandlerError(handler.CampaignDelete))
+	})
 
 	http.ListenAndServe(":8181", router)
 }
